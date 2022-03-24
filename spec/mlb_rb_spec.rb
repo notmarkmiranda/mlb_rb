621fe7e8a4255b296dbb5e394b3e832111c6b4d8
Mark Miranda <mmiranda@doximity.com>
2022-03-24 08:51:47 -0600

    $ bundle gem mlb_rb

---
+++ spec/mlb_rb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MlbRb do
+  it "has a version number" do
+    expect(MlbRb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
